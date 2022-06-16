0e6022f80a21aa478f59d6b3e9fb6918323ce88a
Ádám Hornyák <adam.hornyak.94@outlook.hu>
2022-06-16 07:30:59 +0200

    Final

--- Tesztfeladat/Form1.cs
+++ Tesztfeladat/Form1.cs
@@ -21,10 +21,10 @@ namespace Tesztfeladat
         }
         private void Form1_Load(object sender, EventArgs e)
         {
-            label1.Text = Globalis.kiir;                         //globális
+            label1.Text = Globalis.kiir;                         //konstans
             label1.MaximumSize = new Size(500,0);                //a label szövegtördelése miatt
             label1.AutoSize = true;
-            Control.CheckForIllegalCrossThreadCalls = false;     //tudom hogy delegálttal kellett volna, de akárhogy írtam nem jött össze.
+            Control.CheckForIllegalCrossThreadCalls = false;     //tudom hogy delegálttal kellett volna
         }
         private void buttonGO_Click(object sender, EventArgs e)
         {
@@ -39,22 +39,22 @@ namespace Tesztfeladat
 
         private void Obj_ErrorChanged(object sender, PropertyChangedEventArgs e)
         {
-            if (ertek != null)                                   //megvizsgálom, hogy nem NULL-e az érték
+            string error = obj.ErrorMessage;                     //a DLL error értéke
+            if (error != null)                                   //megvizsgálom, hogy nem NULL-e az érték
             {
-                textBox1.Text += "Hibaüzenet: "+error;
+                textBox1.Text += "Hibaüzenet: "+error;           //ha van akkor kiírja a hibát
                 textBox1.Text += Environment.NewLine;
             }
         }
 
-        private void OnValueChanged(object sender, PropertyChangedEventArgs e) //ezt hívja meg változáskor
+        private void OnValueChanged(object sender, PropertyChangedEventArgs e)
         {
-            
-            textBox2.Text = Convert.ToString(szamlalo);
+            tmp_string = ertek;                                  //ideiglenes string az össze hasonlításhoz
+            ertek = obj.Value;                                   //megkapja a Value értékét.
             if (ertek == null)                                   //megvizsgálom, hogy NULL-e az érték
             {
                 textBox1.Text += "NULL";
                 textBox1.Text += Environment.NewLine;
-                szamlalo++;
             }
             else Formazas();
         }
@@ -65,7 +65,7 @@ namespace Tesztfeladat
             button1.BackColor = Color.Red;
         }
 
-        private void buttonFWRI_Click(object sender, EventArgs e) //fájl íráshoz
+        private void buttonFWRI_Click(object sender, EventArgs e) //fájl írás
         {
             StreamWriter writer = new StreamWriter("adat.txt");
             writer.WriteLine(textBox1.Text);
@@ -74,30 +74,40 @@ namespace Tesztfeladat
 
         void Formazas ()                                       //texbox kimenetelének formázása megadott paraméterek szerint
         {
-            var tmp_string = ertek;                            //ideiglenes string
-            if (ertek.Length == 5)
+            if (tmp_string == ertek) goto vege;                //dupla értékek elkerülése
+            else
             {
-                for (int i = 0; i < tmp_string.Length; i++)
+                if (ertek.Length == 5)
                 {
-                    if (i == 0)
+                    for (int i = 0; i < 1; i++)
                     {
-                        if (char.IsLetter(tmp_string[i]) && char.IsUpper(tmp_string[i])) //ha az első karakter nagybetű akkor 
-                        {                                                                //lefut a maradék 4 karakternek a vizsgálata is 
-                            if (char.IsDigit(tmp_string[i + 1]) && char.IsDigit(tmp_string[i + 1]) && char.IsDigit(tmp_string[i + 1]) && char.IsDigit(tmp_string[i + 1]))
-                            {
-                                textBox1.AppendText(tmp_string + "\t MEGFELELŐ");
-                                szamlalo++;
+                        if (i == 0)
+                        {
+                            if (char.IsLetter(ertek[i]) && char.IsUpper(ertek[i])) //ha az első karakter nagybetű akkor 
+                            {                                                                
+                                if (char.IsDigit(ertek[i + 1]) && char.IsDigit(ertek[i + 1]) && char.IsDigit(ertek[i + 1]) && char.IsDigit(ertek[i + 1])) //lefut a maradék 4 karakternek a vizsgálata is 
+                                {
+                                    textBox1.AppendText(ertek + "   MEGFELELŐ");
+                                    textBox1.Text += Environment.NewLine;
+                                    textBox1.SelectionStart = textBox1.TextLength;  //a textbox végére teker
+                                    textBox1.ScrollToCaret();
+                                    szamlalo++;
+                                    goto vege;                                     //nem elegáns a GOTO de egy függvényben egy IF szerkezeten belül van minden, máshova nem ugrik
+                                }     
                             }
                             else break;
                         }
                         else break;
-                    }
-                    else break;
-                }
-                goto kesz;                                      //nem elegáns a GOTO de egy helyen van minden, máshova nem ugrik
+                    }                                               
+                }                                                   
+                textBox1.AppendText(ertek + "   NEM MEGFELELŐ");
+                textBox1.Text += Environment.NewLine;
+                textBox1.SelectionStart = textBox1.TextLength;
+                textBox1.ScrollToCaret();
+                textBox2.Text = Convert.ToString(szamlalo);
+                szamlalo++;
             }
-            else goto kesz;
-            kesz: textBox1.AppendText(ertek + "\t NEM MEGFELELŐ");
+        vege: textBox2.Text = Convert.ToString(szamlalo);           
         }
 
         void Leall()
@@ -106,43 +116,18 @@ namespace Tesztfeladat
             buttonFWRI.Enabled = true;                        // a fájl írás engedélyezése
         }
 
-        private string ertek;                                // DLL Value értéke
-        public string Ertek
-        {
-            get
-            {
-                return ertek;
-            }
-            set
-            {
-                ertek = obj.Value;                            // itt rakom be a DLL Value értékét
-            }
-        }
-
-        private string error;
-        public string Error
-        {
-            get
-            {
-                return error;
-            }
-            set
-            {
-                error = obj.ErrorMessage;                            // itt rakom be a DLL error message értékét
-            }
-        }
-
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
         }
         private void textBox2_TextChanged(object sender, EventArgs e)
         {
         }
-
-        public GetID.GetID obj = new GetID.GetID();
-        public int szamlalo = 1;                              //kiírt érték számláló
+        private string ertek;                                //DLL Value értéke                            
+        private string tmp_string;                           //ideiglenes string a Value előző értékének
+        public GetID.GetID obj = new GetID.GetID();          //a DLL objektumának hívása
+        public int szamlalo = 0;                             //kiírt érték számláló
     }
-    public  class Globalis                                   // ne zavarjon máshol
+    public  class Globalis                                   // ne zavarjon máshol,konstans
     {
         public const string kiir = 
         "A \"GO\"-gomb lenyomásakor elindul az eljárás, és folyamatosan generál, a \"STOP\"-gombbal pedig leáll!\n\nAz \"F.WRITE\"-gombbal kiírhatjuk a TextBox tartalmát egy \"adat.txt\" nevű fájlba.";
